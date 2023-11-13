73ebd5a4bbf8aceae575e3e1e3270caa0fe95d62
dahye <bbngsk@gmail.com>
2023-11-13 09:43:53 +0900

    Initial Commit

---
+++ SwiftUIStudy/SwiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyApp.swift
+//  SwiftUIStudy
+//
+//  Created by 박다혜 on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
